a7a4f4038f84cf552b4038ef4932cb7f67d7bb38
Rima Bhumbla <105192284+rbhumbla1@users.noreply.github.com>
2023-03-08 16:52:30 -0800

    added more reaction to thoughts data and updated reactions data

--- utils/seed.js
+++ utils/seed.js
@@ -1,60 +1,64 @@
 const connection = require('../config/connection');
-const { User, Thought} = require('../models');
-const {getThought, getReaction, getUser } = require('./data');
+const { User, Thought } = require('../models');
+const { getThought, getReaction, getUser } = require('./data');
 
 connection.on('error', (err) => err);
 
 connection.once('open', async () => {
-  console.log('connected');
+    console.log('connected');
 
-  // Drop existing courses
-  await User.deleteMany({});
+    // Drop existing courses
+    await User.deleteMany({});
 
-  // Drop existing students
-  await Thought.deleteMany({});
+    // Drop existing students
+    await Thought.deleteMany({});
 
-  // Create empty array to hold the users
-const users  = [];
+    // Create empty array to hold the users
+    const users = [];
 
-  // Create empty array to hold the thougths
-  const thoughts  = [];
+    // Create empty array to hold the thougths
+    const thoughts = [];
 
-// Loop 20 times -- add reactions to the reactions array
-for (let i = 0; i < 20; i++) {
-  // Get some random reaction objects using a helper function that we imported from ./data
-  const user = getUser(i);
- const thought = getThought(i);
- const reaction = {reactionText: getReaction(i), username: getUser(i+20)};
+    // Loop 20 times -- add reactions to the reactions array
+    for (let i = 0; i < 20; i++) {
+        // Get some random reaction objects using a helper function that we imported from ./data
+        const user = getUser(i);
+        const thought = getThought(i);
+        const reaction = [{ reactionText: getReaction(i), username: getUser(i + 20).username },
+        { reactionText: getReaction(i + 1), username: getUser(i + 1).username }];
 
-  let thoughtObj = {
-    thoughtText: thought,
-    username: user.username,
-    reactions:[reaction]
-  };
+        let thoughtObj = {
+            thoughtText: thought,
+            username: user.username,
+            reactions: [reaction]
+        };
 
-  thoughts.push(thoughtObj);
+        thoughts.push(thoughtObj);
 
-  let friend1, friend2;
-  friend1 = {username: getUser(i+20).username, email: getUser(i+20).email};
+        let friend1, friend2;
+        friend1 = { username: getUser(i + 20).username, email: getUser(i + 20).email };
 
- friend2 = {username: getUser(i+1).username, email: getUser(i+1).email};
-  
-  users.push({
-    username: user.username,
-    email: user.email,
-    thoughts: [thoughtObj],
-    friends: [friend1, friend2]
-  })
-}
+        friend2 = { username: getUser(i + 1).username, email: getUser(i + 1).email };
 
-console.log(thoughts);
-console.log(users);
+        users.push({
+            username: user.username,
+            email: user.email,
+            thoughts: [thoughtObj],
+            friends: [friend1, friend2]
+        })
+    }
+
+    console.log(thoughts);
+    console.log(users);
 
 
-// Add users to the collection and await the results
-await User.collection.insertMany(users);
+    // Add users to the collection and await the results
+    await User.collection.insertMany(users);
 
-// Add thoughts to the collection and await the results
-await Thought.collection.insertMany(thoughts);
+    // Add thoughts to the collection and await the results
+    await Thought.collection.insertMany(thoughts);
+
+    console.info('Seeding complete! 🌱');
+    process.exit(0);
 
 });
